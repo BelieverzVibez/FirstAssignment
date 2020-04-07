a88b0f401f8cf849a2ac33e99b6f5c7f1de968a0
selva <selva@DESKTOP-QGIA4I3>
2020-04-07 12:50:52 +0530

    committed

---
+++ FirstAssignment/src/packageNameNew/FatherClass.java
@@ -0,0 +1,41 @@
+package packageNameNew;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Utilities.RunnableJavaJar;
+
+public class FatherClass {
+
+	public void noushiq() {
+		System.out.println("first son");
+	}
+	
+	public void santhosh() {
+		System.out.println("second son");
+	}
+	
+	public void karthi() {
+		System.out.println("third son");
+	}
+	
+	public static void main(String[] args) {
+		//objectName.noushiq();
+		
+		/*List<FatherClass> objName = new ArrayList<FatherClass>();
+		FatherClass objectName = new FatherClass();
+
+		objectName.noushiq();
+		objectName.karthi();
+		
+		objName.add(objectName);
+		
+		for(FatherClass u:objName) {
+			System.out.println(u);*/
+		
+		
+		
+		
+		
+	}
+}
---
+++ FirstAssignment/src/packageNameNew/PojaConcept.java
@@ -0,0 +1,20 @@
+package packageNameNew;
+
+public class PojaConcept {
+
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
